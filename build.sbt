3fe2ca92d3b19baea5e4206071f676492351e8e9
Jirka Kremser <jkremser@redhat.com>
2016-08-30 16:29:20 +0200

    Spark demo app

--- build.sbt
+++ build.sbt
@@ -2,11 +2,19 @@ name := "spark-fu"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.10.6"
 
 resolvers += "Spark Packages Repo" at "https://dl.bintray.com/spark-packages/maven"
+resolvers += Resolver.mavenLocal
+//resolvers += "cloudera-repo-releases" at "https://repository.cloudera.com/artifactory/repo"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.0.0",
-  "com.datastax.spark" %% "spark-cassandra-connector" % "1.6.0"
+  "org.apache.spark" %% "spark-sql" % "2.0.0",
+  "org.apache.spark" %% "spark-mllib" % "2.0.0",
+  "org.scalanlp" %% "breeze" % "0.11.2",
+  "org.scalanlp" %% "breeze-macros" % "0.11.2",
+  "com.datastax.spark" %% "spark-cassandra-connector" % "2.0.0-M1",
+  "com.cloudera.sparkts" % "sparkts" % "0.3.0"
+//  "com.cloudera.sparkts" % "sparktimeseries" % "0.1.0"
 )
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,17 +1,59 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import com.datastax.spark.connector._
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.stat.Statistics
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 
 // define main method (scala entry point)
 object App {
+
   def main(args: Array[String]) {
 
-    // initialise spark context
-    val conf = new SparkConf().setAppName("HelloWorld").setMaster("local")
+    // initialize the spark context
+    val conf = new SparkConf()
+      .setAppName("HelloHawkular")
+      .setMaster("local")
+      .set("spark.cassandra.connection.host", "127.0.0.1")
     val sc = new SparkContext(conf)
 
-    // do stuff
-    println("Hello, world!")
+    // this RDD represents the cassandra table
+    val rdd: RDD[CassandraRow] = sc.cassandraTable("hawkular_metrics", "data")
+    println(rdd.count)
+    println(rdd.first)
+
+    // prepare the RDDs from the cassandra SST table by filtering only the relevant data
+    val feedId = "e69a19f7-76e7-4fd2-8ed5-864d1570f3ff"
+    val metric1 = "Total Memory"
+    val metric2 = "Available Memory"
+    val total: RDD[Double] = rdd.filter(x => {
+        val metricId = x.getString("metric")
+        metricId.contains(feedId) && metricId.contains(metric1)
+      })
+      .map(_.getDouble("n_value"))
+      .repartition(8)
+
+    val free: RDD[Double] = rdd.filter(x => {
+        val metricId = x.getString("metric")
+        metricId.contains(feedId) && metricId.contains(metric2)
+      })
+      .map(_.getDouble("n_value"))
+      .repartition(8)
+
+    // create a new RDD based on other ones
+    val used: RDD[Double] = total.zip(free).map(e => e._1 - e._2)
+
+    // calculate the correlation between two RDDs
+    val correlation: Double = Statistics.corr(used, free, "pearson")
+    println(s"Correlation is: $correlation")
+
+    // clustering
+    val usedMemoryVector = used.map(x => Vectors.dense(x))
+    val numClusters = 3
+    val numIterations = 20
+    val clusters: KMeansModel = KMeans.train(usedMemoryVector, numClusters, numIterations)
+    println("Cluster centers:")
+    clusters.clusterCenters.foreach(println)
 
     // terminate spark context
     sc.stop()
